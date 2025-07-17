7019278d4fa03950248ee4007a8542b07fd830dd
KartikSaxena19 <saxenaaashu74@gmail.com>
2025-07-17 16:26:16 +0530

    preparing for deployment

---
+++ app/(main)/video-call/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VideoCall from './video-call-ui';
+
+const VideoCallPage = async({searchParams}) => {
+  const { sessionId, token } = await searchParams;
+
+  return <VideoCall sessionId={sessionId} token={token} />;
+}
+
+export default VideoCallPage
